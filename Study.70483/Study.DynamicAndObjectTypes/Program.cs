d45146277c6678b3a93e090168ccb39ed3ac4031
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-06 20:55:01 -0200

    Adds dynamic and object types exercise

---
+++ Study.70483/Study.DynamicAndObjectTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Study.DynamicAndObjectTypes {
+    class Program {
+        static void Main(string[] args) {
+            var message = new { From = "Jim Morris", To = "Mary North", Content = "Hello World" };
+            SendMessage(message);
+
+            dynamic messageExpando = new ExpandoObject();
+            messageExpando.From = "Expando Morris";
+            messageExpando.To = "Expando North";
+            messageExpando.Content = "Hello Expando";
+            SendMessage(messageExpando);
+
+            Console.ReadKey();
+        }
+
+        private static void SendMessage(dynamic msg) {
+            Console.WriteLine(msg.From);
+            Console.WriteLine(msg.To);
+            Console.WriteLine(msg.Content);
+        }
+    }
+}
