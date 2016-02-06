7dae7d2ced5dfe03787ef73a961a58903e6de25c
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-06 18:00:49 -0200

    Creates the WebClient download jpg file exercise

--- Study.70483/Study.70483/Program.cs
+++ Study.70483/Study.70483/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 
 namespace Study._70483 {
     class Program {
         static void Main(string[] args) {
+            try {
+                WebClient client = new WebClient();
+                client.DownloadFile("https://octodex.github.com/images/baracktocat.jpg", $@"C:\Users\{Environment.UserName}\Pictures\file1.jpg");
+                client.Dispose();
+                Console.WriteLine("File saved...");
+                Console.ReadKey();
+            } catch (Exception exception) {
+                Console.WriteLine(exception);
+                Console.ReadKey();
+            }
         }
     }
 }
