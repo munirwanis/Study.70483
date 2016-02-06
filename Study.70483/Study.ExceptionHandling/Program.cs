ffae17a896d8fd131a8fb1b0948ecd13825373bc
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-06 20:29:46 -0200

    Adds event exception handling exercise

---
+++ Study.70483/Study.ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study.ExceptionHandling {
+    class Program {
+
+        private static Stopwatch _execTimer = new Stopwatch();
+
+        public static void Delay(int delay) => Thread.Sleep(delay);
+
+        public static void LogLongExec(string msg) {
+            if (_execTimer.Elapsed.Seconds >= 5) {
+                throw new Exception($"Execution is too long > {msg} > {_execTimer.Elapsed.TotalMilliseconds}");
+            }
+        }
+
+        static void Main(string[] args) {
+            _execTimer.Start();
+
+            try {
+                Delay(10);
+                LogLongExec("Delay(10)");
+                Delay(5000);
+                LogLongExec("Delay(5000)");
+            } catch (Exception ex) {
+                using (
+                    XmlWriterTraceListener log1 =
+                        new XmlWriterTraceListener($@"C:\Users\{Environment.UserName}\Pictures\Error.log")) {
+                    log1.TraceEvent(new TraceEventCache(), ex.Message, TraceEventType.Error, ex.HResult);
+                    log1.Flush();
+                }
+            }
+        }
+    }
+}
