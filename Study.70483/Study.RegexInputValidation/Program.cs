dacb9f6e1e65e7b65a115f9b369263a737c09163
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-06 20:02:06 -0200

    Adds RegexMatch exercise

---
+++ Study.70483/Study.RegexInputValidation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Study.RegexInputValidation {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(ContainsHyperlink("<a href=\"http://www.w3schools.com/html/\">Visit our HTML tutorial</a>"));
+            Console.ReadKey();
+        }
+
+        static bool ContainsHyperlink(string inputData) {
+            string regExpattern = "href\\s*=\\s*(?:\"(?<1>[^\"]*)\"|(?<1>\\S+))";
+            var evaluator = new Regex(regExpattern, RegexOptions.Compiled);
+            return evaluator.IsMatch(inputData);
+        }
+    }
+}
