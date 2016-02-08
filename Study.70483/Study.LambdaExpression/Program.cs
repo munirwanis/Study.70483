f9dfd4b7bfc9258fdfca0ab8615fb82091f41c47
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-08 18:46:59 -0200

    Adds LambdaExpression exercise

---
+++ Study.70483/Study.LambdaExpression/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.LambdaExpression {
+    class Program {
+        static void Main(string[] args) {
+            List<string> departments = new List<string>
+            {
+                "Accounting", "Marketing", "Sales", "Manufacturing", "Information Systems", "Training"
+            };
+
+
+        }
+
+        private static bool GetMatches(List<string> departments, string searchTerm) {
+            // Original:
+            //var findDepartment = departments.Exists((delegate (string deptName) {
+            //    return deptName.Equals(searchTerm);
+            //}));
+
+            // Response
+            var findDepartment = departments.Exists((deptName => deptName.Equals(searchTerm)));
+            return findDepartment;
+        }
+    }
+}
