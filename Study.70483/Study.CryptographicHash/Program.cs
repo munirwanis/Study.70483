afbe4d19f150c78b3fe8f537d2cddfdac90409f0
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-11 23:16:42 -0200

    Adds hash exercise

---
+++ Study.70483/Study.CryptographicHash/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace Study.CryptographicHash {
+    class Program {
+        static void Main(string[] args) {
+        }
+
+        public static byte[] GenerateHash(string fileName, string hashAlgoritm) {
+            var signatureAlgo = HashAlgorithm.Create(hashAlgoritm);
+            var fileBuffer = File.ReadAllBytes(fileName);
+            return signatureAlgo.ComputeHash(fileBuffer);
+        }
+    }
+}
