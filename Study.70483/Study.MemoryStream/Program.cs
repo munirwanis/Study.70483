4ddbb0aa6d8ff24c22a807c034ed4b1b5aa9bdac
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-11 22:26:46 -0200

    Adds memory stream exercise

---
+++ Study.70483/Study.MemoryStream/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace Study.MemoryStream {
+    class Program {
+        static void Main(string[] args) {
+            var response = WriteName("Something");
+            Console.WriteLine(response);
+            Console.ReadKey();
+        }
+
+        static System.IO.MemoryStream WriteName(string name) {
+            var ms = new System.IO.MemoryStream();
+            var binary = XmlDictionaryWriter.CreateBinaryWriter(ms);
+            var ser = new DataContractSerializer(typeof(string));
+            ser.WriteObject(binary, name);
+            binary.WriteEndDocument();
+            return ms;
+        }
+    }
+}
