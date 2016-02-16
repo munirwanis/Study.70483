51eefd211038572757582b6bc4e11963bfb75d6a
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-15 22:41:10 -0200

    Added date culture exercise

---
+++ Study.70483/Study.ValidatingDate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.ValidatingDate {
+    class Program {
+        static void Main(string[] args) {
+            DateTime utcDate;
+            string localTime = DateTime.Now.ToString("u");
+            ConvertDateToUtc(localTime, out utcDate);
+
+            Console.WriteLine($"Local Time: {localTime}\n" +
+                              $"UTC Time  : {utcDate.ToString("u")}");
+            Console.ReadKey();
+        }
+
+        static bool ConvertDateToUtc(string inputDate, out DateTime validatedDate) {
+            bool validDate = DateTime.TryParse(inputDate, CultureInfo.CurrentCulture,
+                DateTimeStyles.AdjustToUniversal | DateTimeStyles.AssumeUniversal, out validatedDate);
+            return validDate;
+        }
+    }
+}
