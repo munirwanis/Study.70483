0434ec438e5fbc121e44be0279274e0b8de0e5e4
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-17 23:02:46 -0200

    Adds EventLog exercise

---
+++ Study.70483/Study.EventLog/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace Study.EventLog {
+    class Program {
+        static void Main(string[] args) {
+        }
+
+        public void WriteToEventLog(string message, EventLogEntryType eventLogEntryType) {
+            System.Diagnostics.EventLog eventLog = new System.Diagnostics.EventLog {
+                Source = "MySource",
+                EnableRaisingEvents = true
+            };
+            eventLog.WriteEntry(message, eventLogEntryType);
+        }
+    }
+}
