fbb604b124df17e89fe26c638da388739c341856
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-19 07:59:37 -0200

    Adds Casting exercise

---
+++ Study.70483/Study.Casting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Study.Casting {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                Calculate(1.23f);
+                Console.ReadKey();
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                throw new Exception(e.Message);
+            }
+        }
+
+        static void Calculate(float amount) {
+            object amountRef = amount;
+            int balance = (int)(float)amountRef;
+            Console.WriteLine(balance);
+        }
+    }
+}
