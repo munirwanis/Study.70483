a3e55726ca3c8947dd3c512f1d0a8cc759053d47
Munir Wanis <fallen.angel.pp@gmail.com>
2016-02-18 22:37:00 -0200

    Adds format string exercise

---
+++ Study.70483/Study.FormatStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Study.FormatStrings {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(FormatCoins("Munir", 10));
+            Console.ReadKey();
+        }
+
+        static string FormatCoins(string name, int coins) {
+            return String.Format("Player {0}, collected {1} coins.", name, coins.ToString("###0"));
+        }
+    }
+}
